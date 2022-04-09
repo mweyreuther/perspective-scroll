55d50e9b276c3c2e5e0868e253a752b75aba4b11
mweyreuther <mweyreuther@me.com>
2022-04-09 09:58:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vite-tailwind-boilerplate-kacks1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vite-tailwind-boilerplate-kacks1)
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue';
+
+// https://www.freecodecamp.org/news/css-3d-scrolling-on-the-z-axis-92503c3ecf3f/
+</script>
+
+<template>
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+#app {
+  @apply text-center mt-8;
+}
+</style>
